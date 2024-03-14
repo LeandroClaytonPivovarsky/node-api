9c4b95d0f7d79e4b5b99bc23d96e1e8daf22d44c
LeandroClaytonPivovarsky <leandro123clayton@gmail.com>
2024-03-14 12:36:13 -0300

    Arrumados os testes

--- src/termodinamica.test.js
+++ src/termodinamica.test.js
@@ -6,8 +6,10 @@ test('Deve retornar os valores correto', () => {
     const termodinamica = new Termodinamica();
 
     expect(termodinamica.toKelvin(23,'C')).toEqual("296K");
+    expect(termodinamica.toKelvin(32, 'F')).toEqual("273K");
 
     expect(termodinamica.toCelcius(5,'F')).toEqual("-15C");
+    expect(termodinamica.toCelcius(296, 'K')).toEqual("23C");
 
     expect(termodinamica.toFahrenheit(45,'C')).toEqual("113F");
 });
